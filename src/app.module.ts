1c62c211d8946c05fbbd8173a9f74cf8c0e1fa34
MatherLyn <2907681146@qq.com>
2020-01-12 16:48:01 +0800

    前后端合并

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,15 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { UserModule } from './user/user.module'
+import { ServeStaticModule } from '@nestjs/serve-static'
+import { join } from 'path'
 
 @Module({
-  imports: [UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join('__dirname', '..', 'client')
+    }),
+    UserModule
+  ]
 })
 export class AppModule {
   
